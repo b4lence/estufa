0798ff49d94a1ae9cc8793eed43849559931c4fd
b4lence <156260451+b4lence@users.noreply.github.com>
2025-06-26 19:56:00 -0300

    Add files via upload

---
+++ _web/_php/pegarDados.php
@@ -0,0 +1,26 @@
+<?php
+require 'conexao.php';
+
+header('Access-Control-Allow-Origin: *;');
+header('Content-Type: application/json; charset=utf-8;');
+
+try {
+    $dataComeco = $_GET['dataComeco'];
+    $dataFim = $_GET['dataFim'];
+    $codigo = $_SESSION['codigo'];
+
+    $query = "SELECT temperatura, umidade_ar, umidade_solo, reservatorio, data, hora FROM dados WHERE codigo = :codigo AND data BETWEEN :dataComeco AND :dataFim";
+    $stmt = $con->prepare($query);
+
+    $stmt->bindParam(':codigo', $codigo);
+    $stmt->bindParam(':dataComeco', $dataComeco);
+    $stmt->bindParam(':dataFim', $dataFim);
+
+    $stmt->execute();
+
+    $dados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo json_encode($dados);
+} catch (PDOException $e) {
+    echo $e;
+}
