4fd8a8e2b1a959f793a98697c765f646193aaee1
b4lence <156260451+b4lence@users.noreply.github.com>
2025-09-25 23:43:34 -0300

    Delete _web directory

--- _web/_js/config.js
+++
@@ -1,9 +0,0 @@
-const plantDesc = document.getElementById("planta-atual")
-
-async function getPlanta() {
-    const objDados = await callDados();
-    const objPlanta = await callPlanta(objDados[0].planta);
-    plantDesc.innerText = objPlanta[0].nome;
-}
-
-getPlanta();
